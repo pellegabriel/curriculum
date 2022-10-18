6174129023df31bfec1ec30e561fde2b159698c8
unknown <gabypelle12@gmail.com>
2022-10-18 19:53:57 -0300

    IMG PROJECTs

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,11 +4,14 @@ import './portfolio.css'
 const Portfolio = () => {
     return (
         <section id="portfolio">
-            <h5>Portfolio</h5>
+            <h5>My Work</h5>
             <h2>Portfolio</h2>
 
             <div className="container portfolio__container">
                 <article className="portfolio__item">
+                    <div className="portfolio__item-image">
+                        <img src="./assets/IMG1.png" alt=""/>
+                    </div>
                     <h3>Map YPF Points</h3>
                     <div className="portfolio__item-cta">      
                         <a href="https://github.com/pellegabriel/mapa" className="nes-btn is-primary" target='_blank' rel="noopener noreferrer">GitHub</a>
@@ -16,6 +19,9 @@ const Portfolio = () => {
                 </article>
 
                 <article className="portfolio__item">
+                    <div className="portfolio__item-image">
+                        <img src="./assets/IMG3.png" alt=""/>
+                    </div>
                     <h3>Movie Search</h3>
                     <div className="portfolio__item-cta">      
                         <a href="https://github.com/pellegabriel/sp-ike/tree/main/src" className="nes-btn is-primary" target='_blank' rel="noopener noreferrer">GitHub</a>
@@ -23,7 +29,10 @@ const Portfolio = () => {
                 </article>
 
                 <article className="portfolio__item">
-                    <h3>Rick and Morty's Gifs</h3>
+                    <div className="portfolio__item-image">
+                        <img src="./assets/IMG2.png" alt=""/>
+                    </div>
+                    <h3>Rick and Morty's</h3>
                     <div className="portfolio__item-cta">      
                         <a href="https://github.com/pellegabriel/my-app2" className="nes-btn is-primary" target='_blank' rel="noopener noreferrer">GitHub</a>
                     </div>
