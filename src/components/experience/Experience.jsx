1d528aaa723b8ca8c9fe195cfe29da9a5b89020b
pellegabriel <81648480+pellegabriel@users.noreply.github.com>
2023-03-20 22:20:55 -0300

    add experience

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -6,8 +6,7 @@ const Experience = () => {
         <section id="experience">
             <h5>Experience</h5>
             <h2>Experience Front-end</h2>
-
-            <div className="container-experience__container">
+             <div className="container-experience__container">
                     <div className="experience__content">
                         <article className="experience__details">
                             <BsPatchCheckFill /> 
@@ -25,8 +24,6 @@ const Experience = () => {
                             <BsPatchCheckFill /> 
                             <h4>JavaScript</h4>
                         </article>
-                    </div>
-                    <div className="experience__content">
                         <article className="experience__details">
                             <BsPatchCheckFill /> 
                             <h4>Git</h4>
@@ -38,6 +35,41 @@ const Experience = () => {
                         <article className="experience__details">
                             <BsPatchCheckFill /> 
                             <h4>TypeScript</h4>
+                        </article>
+                        <article className="experience__details">
+                            <BsPatchCheckFill /> 
+                            <h4>ES6</h4>
+                        </article>
+                    </div>
+                    <div className="experience__content">
+                       
+                        <article className="experience__details">
+                            <BsPatchCheckFill /> 
+                            <h4>AWS-S3</h4>
+                        </article>
+                        <article className="experience__details">
+                            <BsPatchCheckFill /> 
+                            <h4>Redux</h4>
+                        </article>
+                        <article className="experience__details">
+                            <BsPatchCheckFill /> 
+                            <h4>Lottie Animation</h4>
+                        </article>
+                        <article className="experience__details">
+                            <BsPatchCheckFill /> 
+                            <h4>Styled Components</h4>
+                        </article>
+                        <article className="experience__details">
+                            <BsPatchCheckFill /> 
+                            <h4>React Query</h4>
+                        </article>
+                        <article className="experience__details">
+                            <BsPatchCheckFill /> 
+                            <h4>React Sprin</h4>
+                        </article>
+                        <article className="experience__details">
+                            <BsPatchCheckFill /> 
+                            <h4>Hoc</h4>
                         </article>
                     </div>
             </div>
