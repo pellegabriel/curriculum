ffab5671dca67b00a61b8d74526a995802ef2f4a
pellegabriel <81648480+pellegabriel@users.noreply.github.com>
2023-07-19 20:22:43 -0300

    agregando weeout

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,110 +1,85 @@
 import React from "react";
 import './portfolio.css'
 
+const projects = [
+    {
+      title: "Parallax",
+      iframe: "https://parallax-react-spring.netlify.app",
+      page: "https://parallax-react-spring.netlify.app"
+    },
+
+    {
+      title: "Cripto-Api",
+      iframe: "https://criptapi.netlify.app/",
+      page: "https://criptapi.netlify.app"
+    },
+    {
+        title: "WeeOut",
+        iframe: "https://main.d3tpf1tfcuyy4x.amplifyapp.com/",
+        page: "https://main.d3tpf1tfcuyy4x.amplifyapp.com/"
+      },
+    {
+      title: "Lottie Animation",
+      iframe: "https://planta-grow-up.netlify.app",
+      page: "https://planta-grow-up.netlify.app"
+    },
+    {
+      title: "Menu Animation",
+      iframe: "https://tucumtucum.netlify.app",
+      page: "https://tucumtucum.netlify.app"
+    },
+    {
+      title: "Lottie Animation 2",
+      iframe: "https://planta-grow-up-2.netlify.app",
+      page: "https://planta-grow-up-2.netlify.app"
+    },
+    {
+      title: "TasksList",
+      iframe: "https://tasksli.netlify.app/",
+      page: "https://tasksli.netlify.app/"
+    },
+    {
+      title: "PokeDex",
+      iframe: "https://enciclopoke.netlify.app/",
+      page: "https://enciclopoke.netlify.app"
+    },
+    {
+      title: "Map YPF Points",
+      iframe: "https://mapappoint.netlify.app",
+      page: "https://mapappoint.netlify.app"
+    },
+    {
+      title: "Movie Search",
+      iframe: "https://buskdor.netlify.app/",
+      page: "https://buskdor.netlify.app"
+    },
+    {
+      title: "Rick and Morty's",
+      iframe: "https://rymorty.netlify.app/",
+      page: "https://rymorty.netlify.app"
+    }
+  ];
+  
+
 const Portfolio = () => {
-    return (
-        <section id="portfolio">
-            <h5>My Work</h5>
-            <h2>Portfolio</h2>
-            
-            <div className="container portfolio__container">
-           
-            <article className="portfolio__item">
+  return (
+    <section id="portfolio">
+
+        <div className="portfolio__container">
+            {projects.map((project, index) => (
+                <div className="portfolio__item" key={index}>
+                    <h3>{project.title}</h3>
                     <div className="portfolio__item-image">
-                    <iframe src="https://parallax-react-spring.netlify.app" width="1200" height="1000" allow="fullscreen" title="Cripto-Api"></iframe>
+                        <iframe src={project.iframe} title={project.title}></iframe>
                     </div>
-                    <h3>Parallax</h3>
-                    <div className="portfolio__item-cta">
-                        <a href="https://parallax-react-spring.netlify.app" className="nes-btn is-primary" target='_blank' rel="noopener noreferrer">Page</a>          
-                      </div>
-                </article>
-                <article className="portfolio__item">
-                    <div className="portfolio__item-image">
-                    <iframe src="https://criptapi.netlify.app/" width="1200" height="1000" allow="fullscreen" title="Cripto-Api"></iframe>
-                    </div>
-                    <h3>Cripto-Api</h3>
-                    <div className="portfolio__item-cta">
-                        <a href="https://criptapi.netlify.app" className="nes-btn is-primary" target='_blank' rel="noopener noreferrer">Page</a>          
-                      </div>
-                </article>
-                <article className="portfolio__item">
-                    <div className="portfolio__item-image">
-                    <iframe src="https://planta-grow-up.netlify.app" width="1200" height="1000" allow="fullscreen" title="Cripto-Api"></iframe>
-                    </div>
-                    <h3>Lottie Animation</h3>
-                    <div className="portfolio__item-cta">
-                        <a href="https://planta-grow-up.netlify.app" className="nes-btn is-primary" target='_blank' rel="noopener noreferrer">Page</a>          
-                      </div>
-                </article>
-                <article className="portfolio__item">
-                    <div className="portfolio__item-image">
-                    <iframe src="https://tucumtucum.netlify.app" width="1200" height="1000" allow="fullscreen" title="Cripto-Api"></iframe>
-                    </div>
-                    <h3>Menu Animation</h3>
-                    <div className="portfolio__item-cta">
-                        <a href="https://tucumtucum.netlify.app" className="nes-btn is-primary" target='_blank' rel="noopener noreferrer">Page</a>          
-                      </div>
-                </article>
-                <article className="portfolio__item">
-                    <div className="portfolio__item-image">
-                    <iframe src="https://planta-grow-up-2.netlify.app" width="1200" height="1000" allow="fullscreen" title="Cripto-Api"></iframe>
-                    </div>
-                    <h3>Lottie Animation 2</h3>
-                    <div className="portfolio__item-cta">
-                        <a href="https://planta-grow-up-2.netlify.app" className="nes-btn is-primary" target='_blank' rel="noopener noreferrer">Page</a>          
-                      </div>
-                </article>
-            
-                <article className="portfolio__item">
-                    <div className="portfolio__item-image">
-                    <iframe src="https://tasksli.netlify.app/" width="1200" height="1000" allow="fullscreen" title="TasksList"></iframe>
-                    </div>
-                    <h3>TasksList</h3>
-                    <div className="portfolio__item-cta">
-                        <a href="https://tasksli.netlify.app/" className="nes-btn is-primary" target='_blank' rel="noopener noreferrer">Page</a>          
-                    </div>
-                </article>
-                <article className="portfolio__item">
-                    <div className="portfolio__item-image">
-                    <iframe src="https://enciclopoke.netlify.app/" width="1200" height="1000" allow="fullscreen" title="PokeDex"></iframe>
-                    </div>
-                    <h3>PokeDex</h3>
-                    <div className="portfolio__item-cta">
-                        <a href="https://enciclopoke.netlify.app" className="nes-btn is-primary" target='_blank' rel="noopener noreferrer">Page</a>          
-                   </div>
-                </article>
-                <article className="portfolio__item">
-                    <div className="portfolio__item-image">
-                    <iframe src="https://mapappoint.netlify.app" width="1200" height="1000" allow="fullscreen" title="Mapa"></iframe>
-                    </div>
-                    <h3>Map YPF Points</h3>
-                    <div className="portfolio__item-cta">
-                        <a href="https://mapappoint.netlify.app" className="nes-btn is-primary" target='_blank' rel="noopener noreferrer">Page</a>          
                 </div>
-                </article>
-                <article className="portfolio__item">
-                    <div className="portfolio__item-image">
-                    <iframe src="https://buskdor.netlify.app/" width="1200" height="1000" allow="fullscreen" title="Movie Search"></iframe>
-                    </div>
-                    <h3>Movie Search</h3>
-                    <div className="portfolio__item-cta"> 
-                        <a href="https://buskdor.netlify.app" className="nes-btn is-primary" target='_blank' rel="noopener noreferrer">Page</a>    
-                                </div>
-                </article>
-
-                <article className="portfolio__item">
-                    <div className="portfolio__item-image">
-                    <iframe src="https://rymorty.netlify.app/" width="1200" height="1000" allow="fullscreen" title="Rick and Morty's"></iframe>
-                    </div>
-                    <h3>Rick and Morty's</h3>
-                    <div className="portfolio__item-cta">
-                        <a href="https://rymorty.netlify.app" className="nes-btn is-primary" target='_blank' rel="noopener noreferrer">Page</a>      
-                   </div>
-                </article>
-
-            </div>
+            ))}
+        </div>
         </section>
-    ) 
-}
 
-export default Portfolio
+  );
+};
+
+export default Portfolio;
+
+
