fad278d6bdeb764d895b45cca25826e83df70b59
pellegabriel <81648480+pellegabriel@users.noreply.github.com>
2024-05-14 12:50:04 -0300

    sacando github

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa"
 const HeaderSocials = () => {
   return (
@@ -7,11 +6,6 @@ const HeaderSocials = () => {
       <a className="icon linkedin" href="https://www.linkedin.com/in/gabrielpelle/" target="_blank" rel="noopener noreferrer" >
       <span className="tooltip">Linkedin</span>     
       <FaLinkedin/>
-      </a>
-
-      <a className="icon github" href="https://github.com/pellegabriel?tab=repositories" target="_blank" rel="noopener noreferrer">
-      <span className="tooltip">Github</span>
-       <FaGithub/>
       </a>
     </div>
   )
