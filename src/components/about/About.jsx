0930ca3bfba127ea37529610b0ce2b648199d013
pellegabriel <81648480+pellegabriel@users.noreply.github.com>
2024-05-22 14:50:40 -0300

    sacando more about me

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,27 +1,33 @@
 import React from "react";
-import './about.css'
-
+import "./about.css";
 
 const About = () => {
-    return (
-        <section id="about">
-            <h5>Get To Know</h5>
-            <h2>About Me</h2>
-        <div className="container about__container">
-            <div className="about__content">
-                <p>
-                Hello my Name Is Gabriel. I come from the land called Argentina, more specifically from Santa Cruz / Río Gallegos.
-                I have the qualities of a dedicated subject interested in web development and who in turn loves art. 
-                <br/>
-                I would like to create pages mixing several disciplines, which I am perfecting day by day.
-                <br/>
-                I am more than willing to learn and create new projects, if you are interested and I can be of use to you in some work/task, contact me in the contact section, and I will gladly answer you.
-                </p>
-                <a href="#contact" className="nes-btn is-primary">Let's Talk</a>
-            </div>
+  return (
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      <div className="container about__container">
+        <div className="about__content">
+          <p>
+            Hello my Name Is Gabriel. I come from the land called Argentina,
+            more specifically from Santa Cruz / Río Gallegos. I have the
+            qualities of a dedicated subject interested in web development and
+            who in turn loves art.
+            <br />
+            I would like to create pages mixing several disciplines, which I am
+            perfecting day by day.
+            <br />
+            I am more than willing to learn and create new projects, if
+            you are interested and I can be of use to you in some work/task,
+            contact me in the contact section, and I will gladly answer you.
+          </p>
+          <a href="#contact" className="nes-btn is-primary">
+            Let's Talk
+          </a>
         </div>
-        </section>
-    ) 
-}
+      </div>
+    </section>
+  );
+};
 
-export default About
+export default About;
--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,7 +4,6 @@ const CTA = () => {
     return (
         <div className="cta">
             <a href='/GabrielsResume.pdf' className="nes-btn">Download CV</a>
-            <a href="#contact" className="nes-btn is-primary">MORE ABOUT ME</a>
         </div>
     )
 }
