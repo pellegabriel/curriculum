fa7c1b42009cf330d22277ab1749862e66fc63f0
pelleakronym <gpelle@akronym.cr>
2024-12-11 02:16:35 -0300

    update text

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-// src/App.js
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
-import About from "./components/about/About.jsx";
-import Contact from "./components/contact/Contact.jsx";
-import Experience from "./components/experience/Experience.jsx";
-import Footer from "./components/footer/Footer.jsx";
 import Header from "./components/header/Header.jsx";
-import Portfolio from "./components/portfolio/Portfolio.jsx";
 
 class App extends Component {
   render() {
@@ -15,13 +9,8 @@ class App extends Component {
       <>
         <div id="AppContainer">
           <Router>
-            <Footer />
             <Routes>
               <Route path="/" element={<Header />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/experience" element={<Experience />} />
-              <Route path="/portfolio" element={<Portfolio />} />
-              <Route path="/contact" element={<Contact />} />
             </Routes>
           </Router>
         </div>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,11 +7,17 @@ const Header = () => {
   return (
     <header className="crt">
       <div className=".container header__container">
-        <h5>Hello i am</h5>
+        <h1 style={{
+          fontSize: '2rem'
+        }}>Hello i am</h1>
         <h1>Pelle Gabriel</h1>
         <Img />
         <h3 className="text-light">Front-end Developer</h3>
         <br />
+        <h3 style={{
+          fontSize: '2rem'
+        }}>Turn the page and look at the companies I worked for.</h3>
+
       </div>
     </header>
   );
